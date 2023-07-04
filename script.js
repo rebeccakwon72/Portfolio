9e245a3c22bdb6d58d607d4f14d99afbaade79d4
rebeccakwon72 <rebeccakwon72@gmail.com>
2023-07-04 15:41:07 -0400

    added new pics

---
+++ script.js
@@ -0,0 +1,13 @@
+// /** @type {HTMLElement} **/
+
+// //define variables for major topics
+// const art_btn = document.querySelector(".art");
+// const computer_btn = document.querySelector(".computer");
+// const club_btn = document.querySelector(".clubs");
+
+// //"art_btn" leads to new screen
+// const art_works = document.querySelector(".art_works");
+
+// art_btn.onclick=function(){
+//     art_works.
+// };
